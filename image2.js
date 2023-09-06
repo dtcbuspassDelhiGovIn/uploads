69eb09a0c100e59c12f19fc25391cbd1821183d6
dtcbuspassDelhiGovIn <129287314+dtcbuspassDelhiGovIn@users.noreply.github.com>
2023-09-06 22:16:23 +0530

    Add files via upload

---
+++ image2.js
@@ -0,0 +1,8 @@
+// Assuming you have a JavaScript variable that holds the image URL
+var imageURL = "https://raw.githubusercontent.com/dtcbuspassDelhiGovIn/uploads/4b5cda9bed8989c6b3e134e8a82f21b27cab5470/sign2.jpeg";
+
+// Get a reference to the <img> element
+var imgElement = document.getElementById("signImage");
+
+// Set the src attribute of the <img> element to the value of your JavaScript variable
+imgElement.src = imageURL;
