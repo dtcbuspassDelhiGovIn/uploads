2c3ec728954e50f5960e0589126f2ff4004ed58e
dtcbuspassDelhiGovIn <129287314+dtcbuspassDelhiGovIn@users.noreply.github.com>
2023-09-09 06:58:04 +0530

    deleted image.js

--- image2.js
+++
@@ -1,9 +0,0 @@
-// Assuming you have a JavaScript variable that holds the image URL
-// var imageURL = "https://rawcdn.githack.com/dtcbuspassDelhiGovIn/uploads/b789448096426c6a1906341cc0cd658d6a56da02/sign1.jpeg";
-var imageURL = "https://raw.githubusercontent.com/dtcbuspassDelhiGovIn/uploads/4b5cda9bed8989c6b3e134e8a82f21b27cab5470/sign2.jpeg";
-
-// Get a reference to the <img> element
-var imgElement = document.getElementById("signImage");
-
-// Set the src attribute of the <img> element to the value of your JavaScript variable
-imgElement.src = imageURL;
